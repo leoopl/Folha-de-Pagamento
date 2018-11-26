d9dbf19b17f0e25e762d408bb602a544fb365fcb
Leonardo Leite <leo.leite@hotmail.com>
2018-11-25 22:06:30 -0200

    Add files via upload

--- test/src/test/Empregado.java
+++ test/src/test/Empregado.java
@@ -9,6 +9,8 @@ public class Empregado {
 	private boolean syndicate;
 	private double syndicateTax;
 	private int syndicateID;
+	private double syndicateService;
+	private double salary;
 	
 	public void setName(String name) {
 		this.name = name;
@@ -24,15 +26,27 @@ public class Empregado {
 		return address;
 	}
 	
-	public void setType(String type) {
-		this.type = type;
+	public void setType(int type) {
+		if (type == 0) {
+			this.type = "Hourly";			
+		}else if (type == 1) {
+			this.type = "Salaried";
+		}else if (type == 2) {
+			this.type = "Commissioned";
+		}
 	}
 	public String getType() {
 		return type;
 	}
 	
-	public void setPaymentMethod(String paymentMethod) {
-		this.paymentMethod = paymentMethod;
+	public void setPaymentMethod(int paymentMethod) {
+		if (paymentMethod == 0) {
+			this.paymentMethod = "Check payment by the post office";			
+		}else if (paymentMethod == 1) {
+			this.paymentMethod = "Check payment in hands";
+		}else if (paymentMethod == 2) {
+			this.paymentMethod = "Bank deposit";
+		}
 	}
 	public String getPaymentMethod() {
 		return paymentMethod;
@@ -52,5 +66,42 @@ public class Empregado {
 		return syndicateID;
 	}
 	
-	
+	public boolean getSyndicate() {
+		return syndicate;
 	}
+	public void setSyndicate(boolean syndicate) {
+		this.syndicate = syndicate;
+	}
+	
+	public double getSyndicateTax() {
+		return syndicateTax;
+	}
+	public void setSyndicateTax(double syndicateTax) {
+		this.syndicateTax = syndicateTax;
+	}
+	
+	public double getSyndicateService() {
+		return syndicateService;
+	}
+	public void setSyndicateService(int syndicateService) {
+		this.syndicateService = syndicateService;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public void getStatus() {
+		System.out.println("ID: "+ getID());
+		System.out.println("Name: "+ getName());
+		System.out.println("Address: "+ getAddress());
+		System.out.println("Salary: "+ getSalary());
+		System.out.println("Type: "+ getType());
+		System.out.println("Payment Method: "+ getPaymentMethod());
+		System.out.println("Syndicate: "+ getSyndicate());
+		System.out.println("Syndicate Tax: "+ getSyndicateTax());
+	}
+	} 
--- test/src/test/Menu.java
+++ test/src/test/Menu.java
@@ -1,12 +1,24 @@
 package test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
 	
-	static void add(List<Empregado> employedList, int id) {
+	static boolean list(ArrayList<Empregado> employedList) {
+		if (employedList.isEmpty()) {
+			System.out.println("Nenhum empregrado cadastrado!");
+			return false;
+		} else {
+			for(int i = 0; i < employedList.size(); i++)
+			{
+				System.out.println("ID: " + employedList.get(i).getID() + " - " + employedList.get(i).getName());
+			}
+		}
+		return true;
+	}
+	
+	static void add(ArrayList<Empregado> employedList, int id) {
 		Empregado emp = new Empregado();
 		Scanner x = new Scanner(System.in);
 		System.out.print("Name: ");
@@ -17,90 +29,165 @@ public class Menu {
 		emp.setAddress(theAddress);
 		int input;
 		do{
-			System.out.println("Choose your employed type:\n [0] Hourly \n [1] Salaried \n [2] Commissioned");
+			System.out.println("Choose your employee type:\n [0] Hourly \n [1] Salaried \n [2] Commissioned");
 			input = x.nextInt();
-			switch (input) {
-			case 0:
-				emp.setType("Hourly");//FALTA COLOCAR O PAGAMENTO	
-				break;
-			case 1:
-				emp.setType("Salaried");//FALTA COLOCAR O PAGAMENTO
-				break;
-			case 2:
-				emp.setType("Commissioned");//FALTA COLOCAR O PAGAMENTO
-				break;
-			default:
-				System.out.println("Try Again!!");
-				break;
+			if (input > 2 || input < 0) {
+				System.out.println("Tente de novo!");
+			} else {
+				emp.setType(input);
 			}
-		}while(input > 2);
+		}while(input > 2 || input < 0);
+		System.out.println("Employee Salary: ");
+		double salary = x.nextDouble();
+		emp.setSalary(salary);
 		do{
 			System.out.println("Choose your Payment Method:\n [0] Check payment by the post office \n [1] Check payment in hands \n [2] Bank deposit");
 			input = x.nextInt();
-			switch (input) {
-			case 0:
-				emp.setPaymentMethod("Check payment by the post office");	
-				break;
-			case 1:
-				emp.setPaymentMethod("Check payment in hands");
-				break;
-			case 2:
-				emp.setPaymentMethod("Bank deposit");
-				break;
-			default:
-				System.out.println("Try Again!!");
-				break;
+			if (input > 2 || input < 0) {
+				System.out.println("Tente de novo!");
+			} else {
+				emp.setPaymentMethod(input);
 			}
-		}while(input > 2);
+		}while(input > 2 || input < 0);
+		
+		
 		emp.setID(id);
-
+		emp.getStatus();
 		employedList.add(emp);
 	}
 	
-	static void remove(List<Empregado> employedList) {
-		for(int i = 0; i < employedList.size(); i++)
-		{
-			System.out.println("ID: " + employedList.get(i).getID() + " - " + employedList.get(i).getName());
+	static void remove(ArrayList<Empregado> employedList) {
+		if (list(employedList)) {
+			Scanner x = new Scanner(System.in);
+			System.out.println("id do funcionario que deseja remover: ");
+			int input = x.nextInt();
+			if (input > employedList.size()) {
+				System.out.println("Empregado inexistente!");				
+			} else {
+				employedList.remove(input);
+			}		
+		} else {
+			System.out.println("Não há empregrado para ser removido!");
 		}
-		Scanner x = new Scanner(System.in);
-		System.out.println("id do funcionario que deseja remover: ");
-		int input = x.nextInt();
-		employedList.remove(input);
+	}
+	
+	static void edit(ArrayList<Empregado> employedList) {
+		if (list(employedList)) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("ID do funcionario que deseja motificar: ");
+			int input = in.nextInt();
+			if (input > employedList.size()) {
+				System.out.println("Empregado inexistente!");				
+			} else {
+				int editInput;
+				do {
+					System.out.println("O que você deseja editar: ");
+					System.out.println("[1] Nome");
+					System.out.println("[2] Endereço");
+					System.out.println("[3] Tipo de Empregado");
+					System.out.println("[4] Metodo de pagamento");
+					System.out.println("[5] Sindicato");
+					System.out.println("[6] Taxa sindical");
+					System.out.println("[7] Salvar");
+					
+					editInput = in.nextInt();
+					
+					switch (editInput) {
+					case 1:
+						System.out.println("New name: ");
+						//String newName = in.nextLine();
+						employedList.get(input).setName(in.nextLine());
+						break;
+					case 2:
+						System.out.println("New Address: ");
+						String newAddress = in.nextLine();
+						employedList.get(input).setName(newAddress);
+						break;
+					case 3:
+						System.out.println("Choose your employee type:\n [0] Hourly \n [1] Salaried \n [2] Commissioned");
+						int theType = in.nextInt();
+						if (theType > 2 || theType < 0) {
+							System.out.println("Tipo Invalido");
+						} else {
+							employedList.get(input).setType(theType);
+						}
+						break;
+					case 4:
+						System.out.println("Choose your Payment Method:\n [0] Check payment by the post office \n [1] Check payment in hands \n [2] Bank deposit");
+						int paymentMethod = in.nextInt();
+						if (paymentMethod > 2 || paymentMethod < 0) {
+							System.out.println("Método Invalido");				
+						} else {
+							employedList.get(input).setPaymentMethod(paymentMethod);
+						}
+						break;
+					case 5:
+						System.out.println("Mudar estado de sindicato:\n [1] Sim\n [2] Não");
+						if (in.nextInt() == 1) {
+							if (employedList.get(input).getSyndicate() == false) {
+								employedList.get(input).setSyndicate(true);							
+							}else if (employedList.get(input).getSyndicate() == true) {
+								employedList.get(input).setSyndicate(false);
+							}
+						}break;
+					case 6:
+						if (employedList.get(input).getSyndicate() == true) {
+							System.out.println("Digite a nova taxa sindical: ");
+							employedList.get(input).setSyndicateTax(in.nextDouble());
+						} else {
+							System.out.println("Empregado não participa do sindicato");
+						}
+						break;			
+					case 7:
+						employedList.get(input).getStatus();
+						break;
+					default:
+						System.out.println("Opção invalida!");
+						break;
+					}
+					
+				} while (editInput != 7);
+			}			
+		} else {
+			System.out.println("Não há empregrado para ser editado!");
+		}
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		List<Empregado> employedList = new ArrayList<Empregado>();
+		ArrayList<Empregado> employedList = new ArrayList<Empregado>();
 		int ID = 0;
 
 	    Scanner in = new Scanner(System.in);
 	    int input = 11;
 	    
 	    do{
-	      System.out.println("MENU:");
+	      System.out.println("----------------MENU----------------");
 	      System.out.println("[1] Adição de um empregado");
 	      System.out.println("[2] Remoção de um empregado");
-	      System.out.println("[3] Lançar um Cartão de Ponto");
-	      System.out.println("[4] Lançar um Resultado Venda");
-	      System.out.println("[5] Lançar uma taxa de serviço");
-	      System.out.println("[5] Lançar uma taxa de serviço");
+	      //System.out.println("[3] Lançar um Cartão de Ponto");
+	      //System.out.println("[4] Lançar um Resultado Venda");
+	      //System.out.println("[5] Lançar uma taxa de serviço");
+	      //System.out.println("[5] Lançar uma taxa de serviço");
 	      System.out.println("[6] Alterar detalhes de um empregado");
-	      System.out.println("[7] Rodar a folha de pagamento para hoje");
-	      System.out.println("[8] Undo");
-	      System.out.println("[9] Redo");
-	      System.out.println("[10] Agenda de Pagamento");
-	      System.out.println("[11] Nova Agenda de Pagamento");
+	      //System.out.println("[7] Rodar a folha de pagamento para hoje");
+	      //System.out.println("[8] Undo");
+	      //System.out.println("[9] Redo");
+	      //System.out.println("[10] Agenda de Pagamento");
+	      //System.out.println("[11] Nova Agenda de Pagamento");
+	      System.out.println("[0] Sair");
 
 	      input = in.nextInt();
 
 	      switch(input){
-	        case 1:add(employedList, ID++);break;
-	        case 2:remove(employedList);break;
+	      	case 0: list(employedList); break;
+	        case 1: add(employedList, ID++);break;
+	        case 2: remove(employedList);break;
 	        case 3:System.out.println("3:");break;
 	        case 4:System.out.println("4:");break;
 	        case 5:System.out.println("5:");break;
-	        case 6:System.out.println("6:");break;
+	        case 6: edit(employedList);break;
 	        case 7:System.out.println("7:");break;
 	        case 8:System.out.println("8:");break;
 	        case 9:System.out.println("9:");break;
@@ -108,13 +195,7 @@ public class Menu {
 	        case 11:System.out.println("11:");break;
 	        default:System.out.println("Operação inválida!!");break;
 	      }
-
-	    }while(input < 11);
-	    
-	    for(int i = 0; i < employedList.size(); i++)
-		{
-			System.out.println(employedList.get(i).getID() + " - " + employedList.get(i).getName());
-		}
+	    }while(input != 0);
 
 	}
 }
