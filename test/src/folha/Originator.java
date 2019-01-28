2521df84af75a4b3b18dd5404e51cfccd4dc6734
Leonardo Leite <leo.leite@hotmail.com>
2019-01-28 07:24:14 -0300

    Add files via upload

---
+++ test/src/folha/Originator.java
@@ -0,0 +1,33 @@
+package folha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Originator {
+	
+	private List<Empregado> status;
+	
+	public void setStatus(List<Empregado> saveStatus) {
+		this.status = (List<Empregado>) ((ArrayList) saveStatus).clone();
+	}
+	
+	public List<Empregado> getStatus() {
+		return this.status;
+	}
+	
+	public Memento saveMementoStatus() {
+		return new Memento(status);
+	}
+	
+	public void restore(List<Empregado> m) {
+		status = m;
+	}
+
+	@Override
+	public String toString() {
+		return "Originator [status=" + status + "]";
+	}
+	
+	
+}
+
